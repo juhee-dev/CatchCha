66b189d62d3c65b7d40d9241a62ae1e678a2edef
Juhee <viimv2000@gmail.com>
2022-11-12 22:38:42 +0900

    Initial Commit

---
+++ CatchCha/Map/PartyloadingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PartyloadingViewController.swift
+//  CatchCha
+//
+//  Created by juhee on 2022/11/12.
+//
+
+import UIKit
+
+class PartyloadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
